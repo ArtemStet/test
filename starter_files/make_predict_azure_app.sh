63f86c41f3f47c6f4f6c44b770468849efcccef0
Artem Stetsenko <83006335+ArtemStet@users.noreply.github.com>
2022-07-21 15:57:57 +0200

    Update make_predict_azure_app.sh

--- starter_files/make_predict_azure_app.sh
+++ starter_files/make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://mywebapp7770000.azurewebsites.net:$PORT/predict 
+     -X POST https://mywebapp7770000.azurewebsites.net/:$PORT/predict 
     
